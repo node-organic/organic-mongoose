b1515754579cc13e15322378b1532bbb898e8f2f
Georgi Yonchev <georgi@yonchev.org>
2015-03-07 17:43:40 +0200

    added support for extended database options
    * updated impl, readme and test spec

--- README.md
+++ README.md
@@ -7,13 +7,28 @@ The organelle provides support for mongoose ORM.
     {
       reactOn: String, /* optional */,
       database: {
-        name: String
+        host: String, /* optional */
+        name: String, /* required */
+        port: Number, /* optional */
+        options: Object /* optional */
       },
       recreateDatabase: Boolean, /* optional */
     }
 
 - `reactOn` - Type of chemical
+- `database.host` - connection_string, mongodb://uri or the host to which you are connecting (default: localhost)
 - `database.name` - name of the database to connect and/or create
+- `database.port` - database port
+- `database.options` - options is a hash with the following possible properties:
+ - db      - passed to the connection db instance
+ - server  - passed to the connection server instance(s)
+ - replset - passed to the connection ReplSet instance
+ - user    - username for authentication
+ - pass    - password for authentication
+ - auth    - options for authentication (see http://mongodb.github.com/node-mongodb-native/api-generated/db.html#authenticate)
+
+  for more info see http://mongoosejs.com/docs/api.html#connection_Connection-open
+
 - `reacreateDatabase` - when set upon first connection will clean all the database records.
 
 ## emits chemical with type `Mongoose`
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports = function OrganicMongoose(plasma, config){
 
   this.config = config
   this.config.emitReady = config.emitReady || "Mongoose"
+  this.config.database.host = this.config.database.host || "localhost"
 
   this.emit = function(type) {
     plasma.emit(type)
@@ -19,7 +20,12 @@ module.exports = function OrganicMongoose(plasma, config){
 
 module.exports.prototype.connect = function(c, next){
   var self = this
-  mongoose.connect('localhost', self.config.database.name, function(err){
+  mongoose.connect(self.config.database.host,
+    self.config.database.name,
+    self.config.database.port ?
+    self.config.database.port : self.config.database.options ?
+    self.config.database.options : {},
+  function(err){
     if (err) {
       console.error(err)
       return next && next(err)
@@ -29,10 +35,15 @@ module.exports.prototype.connect = function(c, next){
       mongoose.connection.db.dropDatabase(function(){
         // workaround mongoose.connection issue with dropped db and open connection
         mongoose.connection.db.close(function(){
-          mongoose.connection.readyState = 0 
-          mongoose.connect('localhost', self.config.database.name, function(err){
+          mongoose.connection.readyState = 0
+          mongoose.connect(self.config.database.host,
+            self.config.database.name,
+            self.config.database.port ?
+            self.config.database.port : self.config.database.options ?
+            self.config.database.options : {},
+          function(err){
             if(err) {
-              console.error(err) 
+              console.error(err)
               return next && next(err)
             }
             self.emit(self.config.emitReady)
